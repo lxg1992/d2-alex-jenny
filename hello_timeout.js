1e9b56317942d6dd0dc05fd16ac356ce8b53a199
Jenny Nguy <j.nguy809@gmail.com>
2019-09-24 16:18:53 +0000

    hello there world

---
+++ hello_timeout.js
@@ -0,0 +1,11 @@
+setTimeout( function() {
+  console.log('4311o')}, 1000
+)
+
+setTimeout( function() {
+  console.log('th3r3')}, 2000
+)
+
+setTimeout( function() {
+  console.log('w0r1d')}, 3000
+)
