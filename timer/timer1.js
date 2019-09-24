b2ec623e28fb5d5b3f505e01b053b24c4341f3e2
Jenny Nguy <j.nguy809@gmail.com>
2019-09-24 17:16:42 +0000

    edge cases

--- timer/timer1.js
+++ timer/timer1.js
@@ -1,11 +1,11 @@
 let timeArray = process.argv.slice(2).sort(function(a, b) {
   return a - b;
-})
+});
 
 for (let time of timeArray) {
-  if (time > 0) {
+  if (time >= 0) {
     setTimeout(() => {
       process.stdout.write('. \n');
-    }, time * 1000)
+    }, time * 1000);
   }
 }
